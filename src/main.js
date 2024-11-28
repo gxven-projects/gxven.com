d2c07cd7d34ff146b158f16aaa2f099c612ebd7f
Squalish <96146016+Squalish@users.noreply.github.com>
2024-11-28 18:45:11 +0100

    god damn it man. of course as soon as i commit everything breaks suddenly. i hope THIS fixes the problem.

--- src/main.js
+++ src/main.js
@@ -83,31 +83,3 @@ function tabHandler(e, tabButtons) {
         }
       });
     });
-
-function playOpenAnimation() {
-  var mainwindow = document.getElementById('main-window');
-  mainwindow.style.animation = 'windowopen 0.20s';
-
-  mainwindow.addEventListener('animationend', () => {
-    mainwindow.style.animation = '';
-  });
-}
-
-function playCloseAnimation() {
-  var mainwindow = document.getElementById('main-window');
-  mainwindow.style.animation = 'windowclose 0.075s';
-
-  mainwindow.addEventListener('animationend', () => {
-    mainwindow.style.animation = '';
-  });
-}
-// GoTo Functions
-async function goHome() {
-  await playCloseAnimation();
-  window.location.href = '../index.html';
-}
-
-async function goTo(location) {
-  await playCloseAnimation();
-  window.location.href = location;
-}
