41d193dd3d2a475da24fe70cb7fe7a114d2e0b40
Squalish <96146016+Squalish@users.noreply.github.com>
2024-12-14 02:59:53 +0100

    i basically squashed every single html file into one and uhh yeah hope everything works now :3 (i swear to god if it doesnt.... ILL GET ULTRAANGRYYY >:CCCC GRRRR!@#!!@#!@#@#)

--- src/control.js
+++ src/control.js
@@ -6,22 +6,32 @@ function playOpenAnimation() {
       mainwindow.style.animation = '';
     });
 }
-  
-function playCloseAnimation() {
-  var mainwindow = document.getElementById('main-window');
-  mainwindow.style.animation = 'windowclose 0.075s';
 
+// GoTo Functions
+const windows = [
+  "welcome",
+  "projects",
+  "socials",
+  "faq",
+  "credits"
+]
+
+let currentwindow = null
+
+function goTo(location) {
+  // ANIMATION STUFF
+  let mainwindow = document.getElementById('main-window');
+  mainwindow.style.animation = 'windowclose 0.075s';
   mainwindow.addEventListener('animationend', () => {
     mainwindow.style.animation = '';
-  });
-}
-// GoTo Functions
-async function goHome() {
-  await playCloseAnimation();
-  window.location.href = '../index.html';
-}
 
-async function goTo(location) {
-  await playCloseAnimation();
-  window.location.href = location;
+    // HIDE ALL WINDOWS
+    for (let i = 0; i < windows.length; i++) {
+      document.getElementById(windows[i]).style.display = 'none';
+    }
+
+    // SHOW THE ONE IT NEEDS TO BE DISPLAYED
+    currentwindow = document.getElementById(windows[location]);
+    currentwindow.style.display = 'block';
+  });
 }
--- src/mobilesupport.js
+++ src/mobilesupport.js
@@ -2,11 +2,21 @@ function isMobile() {
     return window.matchMedia("(max-width: 540px)").matches;
 }
 
-const meow = document.getElementById("main");
+const meow = [
+    "win1",
+    "win2",
+    "win3",
+    "win4",
+    "win5",
+];
 
 if (isMobile()) {
     console.log("You are on a mobile device!");
-    meow.style.transform  = "scale(0.70, 0.70)"
+    for (let i = 0; i < meow.length; i++) {
+        {
+            document.getElementById(meow[i]).style.transform  = "scale(0.70, 0.70)";
+        }
+    }
 } else {
     console.log("You are on a desktop device!");
 }
