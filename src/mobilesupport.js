4759866de30002305139fba37993406c4e965d60
Squalish <96146016+Squalish@users.noreply.github.com>
2025-03-18 01:58:29 +0100

    - Rewrote the way windows worked - Socials got renamed to Platforms - Updated profile pictures on Platforms - Removed unused files - Edited some text

--- src/mobilesupport.js
+++ src/mobilesupport.js
@@ -10,17 +10,22 @@ const meow = [
     "win5",
 ];
 
-if (isMobile()) {
-    console.log("You are on a mobile device!");
-    for (let i = 0; i < meow.length; i++) {
-        {
-            const content = document.getElementById("content");
-            content.style.transform  = "scale(0.70, 0.70)";
-            content.style.position = 'absolute';
-            content.style.top = '50%';
-            content.style.left = '50%';
+function check()
+{
+    if (isMobile()) {
+        console.log("You are on a mobile device!");
+        for (let i = 0; i < meow.length; i++) {
+            {
+                const content = document.getElementById("content");
+                content.style.transform  = "scale(0.70, 0.70)";
+                content.style.position = 'absolute';
+                content.style.top = '50%';
+                content.style.left = '50%';
+            }
         }
+    } else {
+        console.log("You are on a desktop device!");
     }
-} else {
-    console.log("You are on a desktop device!");
 }
+
+check();
